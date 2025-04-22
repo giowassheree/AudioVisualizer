d454750e81ae62fc29e2c34d767c166aa04823a2
giowassheree <tul40930@temple.edu>
2025-04-22 13:20:13 -0400

    implemented callback to server

--- backend/spotify-web-player/server/index.js
+++ backend/spotify-web-player/server/index.js
@@ -48,3 +48,29 @@ router.get('/auth/login', (req, res) => {
   
     res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
  })
+
+ app.get('/auth/callback', (req, res) => {
+
+    var code = req.query.code;
+  
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: "http://127.0.0.1:3000/auth/callback",
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64')),
+        'Content-Type' : 'application/x-www-form-urlencoded'
+      },
+      json: true
+    };
+  
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var access_token = body.access_token;
+        res.redirect('/')
+      }
+    });
+})
