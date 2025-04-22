32e43ebc04e1267585e5946579d85b80ef26cf10
giowassheree <tul40930@temple.edu>
2025-04-22 13:10:48 -0400

    added state and user authorization request

--- backend/spotify-web-player/server/index.js
+++ backend/spotify-web-player/server/index.js
@@ -19,3 +19,32 @@ app.get('/auth/callback', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening at http://127.0.0.1:${port}`)
 })
+
+var generateRandomString = function (length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+
+router.get('/auth/login', (req, res) => {
+
+    var scope = "streaming \
+                 user-read-email \
+                 user-read-private"
+  
+    var state = generateRandomString(16);
+  
+    var auth_query_parameters = new URLSearchParams({
+      response_type: "code",
+      client_id: spotify_client_id,
+      scope: scope,
+      redirect_uri: "http://127.0.0.1:3000/auth/callback",
+      state: state
+    })
+  
+    res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
+ })
